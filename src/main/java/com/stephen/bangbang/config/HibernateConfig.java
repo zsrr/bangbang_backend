a13a9cf222ccfdd1f2c83f8291e28af860d01fbe
zsr <zsr13259726721@gmail.com>
2017-08-01 17:05:02 +0800

    Cache config added

--- src/main/java/com/stephen/bangbang/config/HibernateConfig.java
+++ src/main/java/com/stephen/bangbang/config/HibernateConfig.java
@@ -4,7 +4,6 @@ import org.hibernate.SessionFactory;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
-import org.springframework.core.io.ClassPathResource;
 import org.springframework.orm.hibernate5.HibernateTransactionManager;
 import org.springframework.orm.hibernate5.LocalSessionFactoryBean;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
